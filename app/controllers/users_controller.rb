6a22f74df4911179977a5a60f5fc16f9e251f118
EC2 Default User <ec2-user@ip-172-31-16-120.ap-southeast-1.compute.internal>
2020-01-03 08:02:58 +0000

    5-5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    # debugger # インスタンス変数を定義した直後にこのメソッドが実行される。
+  end
+  
+  
   def new
+    @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入する。
   end
 end
