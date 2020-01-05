cd1ab1cfed92b10d3200964a3fe5884a2ca6eada
EC2 Default User <ec2-user@ip-172-31-16-120.ap-southeast-1.compute.internal>
2020-01-05 05:03:30 +0000

    7-1-4

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+
+  # 永続セッションを記憶します(Userモデルを参照)
+  def remenber(user)
+    user.remenber
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remenber_token] = user.remenber_token
+  end
+
   
   # セッションと@current_usreを破棄します
   def log_out
@@ -13,10 +21,17 @@ module SessionsHelper
   end
 
 
-  # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
+  # 一時的セッションにいるユーザーを返します。
+  # それ以外の場合はcookiesに対応するユーザーを返します。
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  # 「remenber＿token」という仮想の属性を作成する
+  attr_accessor :remenber_token
   before_save { self.email = email.downcase }
   
   validates :name,  presence: true, length: { maximum: 50 }
@@ -8,4 +10,32 @@ class User < ApplicationRecord
                     uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  
+  # 渡された文字列のハッシュ値を返します
+  def User.digest(string)
+    cost =
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
+  
+  # ランダムなトークンを返す
+  def User.new_token
+    SesureRandom.urlsafe_base64
+  end
+
+  # 永続セッションの為ハッシュ化したトークンをデータベースに記憶します
+  def remenber
+    self.remenber_token = User.new_token
+    update_attribute(:remenber_digest, User.digest(remenber_token))
+  end
+
+  # トークンがダイジェストと一致すればtrueを返します
+  def authenticate?(remenber_token)
+    BCrypt::Password.new(remenber_digest).is_password?(remenber_token)
+  end
 end
